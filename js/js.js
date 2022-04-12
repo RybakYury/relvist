596e7f222f2a4c3367606667072da80616769780
Yury <31966973+RybakYury@users.noreply.github.com>
2022-04-12 22:49:46 +0300

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -1,6 +1,30 @@
-
-<script>
-AOS.init({
-	duration: 1200,
-  })
-</script>
+var time = 2,
+  cc = 1;
+$(window).scroll(function() {
+  $('#counter').each(function() {
+    var
+      cPos = $(this).offset().top,
+      topWindow = $(window).scrollTop();
+    if (cPos < topWindow + 400) {
+      if (cc < 2) {
+        $(".number").addClass("viz");
+        $('div').each(function() {
+          var
+            i = 1,
+            num = $(this).data('num'),
+            step = 100 * time / num,
+            that = $(this),
+            int = setInterval(function() {
+              if (i <= num) {
+                that.html(i);
+              } else {
+                cc = cc + 2;
+                clearInterval(int);
+              }
+              i++;
+            }, step);
+        });
+      }
+    }
+  });
+});
